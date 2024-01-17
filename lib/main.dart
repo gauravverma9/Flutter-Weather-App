fecbe66d4b2fe35c405c1b7984cee4522150ae8b
Flutter-Weather-App <gauravbagga955@gmail.com>
2024-01-17 12:29:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:open_weather/screens/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Center(child: Text("Weather App")),
+//         ),
+//         body:
+//             home_screen() // This trailing comma makes auto-formatting nicer for build methods.
+//         );
+//   }
+// }
---
+++ lib/screens/constants.dart
@@ -0,0 +1,2 @@
+const String domain = 'https://api.openweathermap.org/data/2.5/weather?';
+const String apiKey = '1862c61f79aea34a7aab3091647f5e02';
